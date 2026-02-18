cbb1e6b2cb647e2a2654603c9da460289766f058
Dharvipatel10 <dharvipatel1610@gmail.com>
2026-02-18 13:23:20 +0100

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,547 @@
+const navLinks = [
+    {
+        id: 1,
+        name: "Projects",
+        type: "finder",
+    },
+    {
+        id: 3,
+        name: "Contact",
+        type: "contact",
+    },
+    {
+        id: 4,
+        name: "Resume",
+        type: "resume",
+    },
+];
+
+const navIcons = [
+    {
+        id: 1,
+        img: "/icons/wifi.svg",
+    },
+    {
+        id: 2,
+        img: "/icons/search.svg",
+    },
+    {
+        id: 3,
+        img: "/icons/user.svg",
+    },
+    {
+        id: 4,
+        img: "/icons/mode.svg",
+    },
+];
+
+const dockApps = [
+    {
+        id: "finder",
+        name: "Portfolio", // was "Finder"
+        icon: "finder.png",
+        canOpen: true,
+    },
+    {
+        id: "safari",
+        name: "Experience", // was "Safari"
+        icon: "safari.png",
+        canOpen: true,
+    },
+    {
+        id: "photos",
+        name: "Gallery", // was "Photos"
+        icon: "photos.png",
+        canOpen: true,
+    },
+    {
+        id: "contact",
+        name: "Contact", // or "Get in touch"
+        icon: "contact.png",
+        canOpen: true,
+    },
+    {
+        id: "terminal",
+        name: "Skills", // was "Terminal"
+        icon: "terminal.png",
+        canOpen: true,
+    },
+    {
+        id: "trash",
+        name: "Archive", // was "Trash"
+        icon: "trash.png",
+        canOpen: false,
+    },
+];
+
+const blogPosts = [
+    {
+        id: 1,
+        date: "Sep 2, 2025",
+        title:
+            "TypeScript Explained: What It Is, Why It Matters, and How to Master It",
+        image: "/images/blog1.png",
+        link: "https://jsmastery.com/blog/typescript-explained-what-it-is-why-it-matters-and-how-to-master-it",
+    },
+    {
+        id: 2,
+        date: "Aug 28, 2025",
+        title: "The Ultimate Guide to Mastering Three.js for 3D Development",
+        image: "/images/blog2.png",
+        link: "https://jsmastery.com/blog/the-ultimate-guide-to-mastering-three-js-for-3d-development",
+    },
+    {
+        id: 3,
+        date: "Aug 15, 2025",
+        title: "The Ultimate Guide to Mastering GSAP Animations",
+        image: "/images/blog3.png",
+        link: "https://jsmastery.com/blog/the-ultimate-guide-to-mastering-gsap-animations",
+    },
+];
+
+const techStack = [
+    {
+        category: "Frontend",
+        items: ["HTML", "JavaScript (ES6+)", "React.js", "Next.js", "TypeScript", "Angular 2+"],
+    },
+    {
+        category: "Frameworks",
+        items: ["FAST API", "Laravel", "Django"],
+    },
+    {
+        category: "Styling",
+        items: ["Tailwind CSS", "Sass", "CSS", "vite"],
+    },
+    {
+        category: "Backend",
+        items: ["Node.js", "Python", "PHP"],
+    },
+    {
+        category: "Database",
+        items: ["MySQL", "MongoDB", "PostgreSQL"],
+    },
+    {
+        category: "Dev Tools",
+        items: ["Git", "GitHub", "Docker", "CI/CD Pipeline", "Agile Methodology"],
+    },
+];
+
+const socials = [
+    {
+        id: 1,
+        text: "Github",
+        icon: "/icons/github.svg",
+        bg: "#f4656b",
+        link: "https://github.com/Dharvipatel10",
+    },
+    {
+        id: 2,
+        text: "LinkedIn",
+        icon: "/icons/linkedin.svg",
+        bg: "#05b6f6",
+        link: "https://www.linkedin.com/in/dharvi-patel10/",
+    },
+];
+
+const photosLinks = [
+    {
+        id: 1,
+        icon: "/icons/gicon1.svg",
+        title: "Library",
+    },
+    {
+        id: 2,
+        icon: "/icons/gicon2.svg",
+        title: "Memories",
+    },
+    {
+        id: 3,
+        icon: "/icons/file.svg",
+        title: "Places",
+    },
+    {
+        id: 4,
+        icon: "/icons/gicon4.svg",
+        title: "People",
+    },
+    {
+        id: 5,
+        icon: "/icons/gicon5.svg",
+        title: "Favorites",
+    },
+];
+
+const gallery = [
+    {
+        id: 1,
+        img: "/images/gal1.png",
+    },
+    {
+        id: 2,
+        img: "/images/gal2.png",
+    },
+    {
+        id: 3,
+        img: "/images/gal3.png",
+    },
+    {
+        id: 4,
+        img: "/images/gal4.png",
+    },
+];
+
+export {
+    navLinks,
+    navIcons,
+    dockApps,
+    blogPosts,
+    techStack,
+    socials,
+    photosLinks,
+    gallery,
+};
+
+const WORK_LOCATION = {
+    id: 1,
+    type: "work",
+    name: "Work",
+    icon: "/icons/work.svg",
+    kind: "folder",
+    children: [
+        // ▶ Project 1
+        {
+            id: 5,
+            name: "IntelliBot",
+            icon: "/images/folder.png",
+            kind: "folder",
+            position: "top-10 left-5", // icon position inside Finder
+            windowPosition: "top-[5vh] left-5", // optional: Finder window position
+            children: [
+                {
+                    id: 1,
+                    name: "IntelliBot.txt",
+                    icon: "/images/txt.png",
+                    kind: "file",
+                    fileType: "txt",
+                    position: "top-5 left-10",
+                    description: [
+                        "IntelliBot is a full-stack AI voice assistant that turns conversations into a real-time interactive experience.",
+                        "It captures speech, processes it using ChatGPT, and responds with natural voice synthesis powered by ElevenLabs.",
+                        "Rather than a traditional chatbot, it feels like talking to a smart digital companion.",
+                        "Built with FastAPI and React, it showcases modern AI integration, RESTful architecture, and seamless voice interaction pipelines."
+                    ],
+                },
+                {
+                    id: 2,
+                    name: "intelliBot.com",
+                    icon: "/images/safari.png",
+                    kind: "file",
+                    fileType: "url",
+                    href: "https://github.com/Dharvipatel10/IntelliBot",
+                    position: "top-10 right-20",
+                },
+                {
+                    id: 4,
+                    name: "IntelliBot.png",
+                    icon: "/images/image.png",
+                    kind: "file",
+                    fileType: "img",
+                    position: "top-52 right-80",
+                    imageUrl: "/images/project-IntelliBot.png",
+                },
+                {
+                    id: 5,
+                    name: "Design.fig",
+                    icon: "/images/plain.png",
+                    kind: "file",
+                    fileType: "fig",
+                    href: "https://google.com",
+                    position: "top-60 right-20",
+                },
+            ],
+        },
+
+        // ▶ Project 2
+        {
+            id: 6,
+            name: "AI Resume Analyzer",
+            icon: "/images/folder.png",
+            kind: "folder",
+            position: "top-52 right-80",
+            windowPosition: "top-[20vh] left-7",
+            children: [
+                {
+                    id: 1,
+                    name: "AI Resume Analyzer Project.txt",
+                    icon: "/images/txt.png",
+                    kind: "file",
+                    fileType: "txt",
+                    position: "top-5 right-10",
+                    description: [
+                        "AI Resume Analyzer is a smart tool that helps you perfect your resume with instant feedback.",
+                        "Instead of guessing what recruiters want, you get AI-powered insights on keywords, formatting, and overall impact.",
+                        "Think of it like having a career coach—pointing out strengths, fixing weaknesses, and boosting your chances of landing interviews.",
+                        "It's built with React and Tailwind, and Puter.js so it runs fast, looks professional, and works seamlessly on any device.",
+                    ],
+                },
+                {
+                    id: 2,
+                    name: "ai-resume-analyzer.com",
+                    icon: "/images/safari.png",
+                    kind: "file",
+                    fileType: "url",
+                    href: "https://github.com/Dharvipatel10/Resume_Analayzer",
+                    position: "top-20 left-20",
+                },
+                {
+                    id: 4,
+                    name: "ai-resume-analyzer.png",
+                    icon: "/images/image.png",
+                    kind: "file",
+                    fileType: "img",
+                    position: "top-52 left-80",
+                    imageUrl: "/images/project-2.png",
+                },
+                {
+                    id: 5,
+                    name: "Design.fig",
+                    icon: "/images/plain.png",
+                    kind: "file",
+                    fileType: "fig",
+                    href: "https://google.com",
+                    position: "top-60 left-5",
+                },
+            ],
+        },
+
+        // ▶ Project 3
+        {
+            id: 7,
+            name: "Python Background Job Engine",
+            icon: "/images/folder.png",
+            kind: "folder",
+            position: "top-10 left-80",
+            windowPosition: "top-[33vh] left-7",
+            children: [
+                {
+                    id: 1,
+                    name: "Python Background Job Engine.txt",
+                    icon: "/images/txt.png",
+                    kind: "file",
+                    fileType: "txt",
+                    position: "top-5 left-10",
+                    description: [
+                        "The FastAPI Background Job Engine is a compact, production-style backend prototype designed to demonstrate how modern systems handle asynchronous workloads.",
+                        "Instead of blocking API requests during heavy processing, it instantly validates structured data and offloads tasks to background workers using Redis and RQ.",
+                        "Think of it as a simplified version of how real AI platforms and automation systems process jobs behind the scenes.",
+                        "Built with FastAPI, Pydantic, Redis, and RQ, it showcases scalable backend architecture, clean data validation, and non-blocking execution patterns."
+                    ],
+                },
+                {
+                    id: 2,
+                    name: "python-background-job-processing.com",
+                    icon: "/images/safari.png",
+                    kind: "file",
+                    fileType: "url",
+                    href: "https://github.com/Dharvipatel10/python-background-job-processing",
+                    position: "top-10 right-20",
+                },
+                {
+                    id: 4,
+                    name: "python-background-job-processing.png",
+                    icon: "/images/image.png",
+                    kind: "file",
+                    fileType: "img",
+                    position: "top-52 right-80",
+                    imageUrl: "/images/project-3.png",
+                },
+                {
+                    id: 5,
+                    name: "Design.fig",
+                    icon: "/images/plain.png",
+                    kind: "file",
+                    fileType: "fig",
+                    href: "https://google.com",
+                    position: "top-60 right-20",
+                },
+            ],
+        },
+
+        // ▶ Project 4
+        {
+            id: 8,
+            name: "HustleHub",
+            icon: "/images/folder.png",
+            kind: "folder",
+            position: "top-10 left-15", // icon position inside Finder
+            windowPosition: "top-[37vh] left-5", // optional: Finder window position
+            children: [
+                {
+                    id: 1,
+                    name: "HustleHub.txt",
+                    icon: "/images/txt.png",
+                    kind: "file",
+                    fileType: "txt",
+                    position: "top-5 left-10",
+                    description: [
+                        "HustleHub is a modern Job Portal built using Laravel (PHP) and React, created to manage job listings, applications, and administrative workflows efficiently.",
+                        "It features a clean RESTful API architecture that separates frontend and backend for better scalability and maintainability.",
+                        "The UI is built with React, TailwindCSS, and Vite—delivering smooth performance, responsive design, and a developer-friendly workflow.",
+                        "Laravel powers authentication, authorization, database operations, and business logic using structured MVC principles.",
+                        "Designed like a real-world hiring system, HustleHub combines performance, security, and scalability into one seamless platform.",
+                    ],
+                },
+                {
+                    id: 2,
+                    name: "HustleHub.com",
+                    icon: "/images/safari.png",
+                    kind: "file",
+                    fileType: "url",
+                    href: "https://github.com/Dharvipatel10/HustleHub",
+                    position: "top-10 right-20",
+                },
+                {
+                    id: 4,
+                    name: "HustleHub.png",
+                    icon: "/images/image.png",
+                    kind: "file",
+                    fileType: "img",
+                    position: "top-52 right-80",
+                    imageUrl: "/images/project-IntelliBot.png",
+                },
+                {
+                    id: 5,
+                    name: "Design.fig",
+                    icon: "/images/plain.png",
+                    kind: "file",
+                    fileType: "fig",
+                    href: "https://google.com",
+                    position: "top-60 right-20",
+                },
+            ],
+        },
+    ],
+};
+
+const ABOUT_LOCATION = {
+    id: 2,
+    type: "about",
+    name: "About me",
+    icon: "/icons/info.svg",
+    kind: "folder",
+    children: [
+        {
+            id: 1,
+            name: "me.png",
+            icon: "/images/image.png",
+            kind: "file",
+            fileType: "img",
+            position: "top-10 left-5",
+            imageUrl: "/images/Dharvi.JPG",
+        },
+        {
+            id: 2,
+            name: "casual-me.png",
+            icon: "/images/image.png",
+            kind: "file",
+            fileType: "img",
+            position: "top-28 right-72",
+            imageUrl: "/images/Dharvi-2.jpg",
+        },
+        {
+            id: 3,
+            name: "conference-me.png",
+            icon: "/images/image.png",
+            kind: "file",
+            fileType: "img",
+            position: "top-52 left-80",
+            imageUrl: "/images/Dharvi-3.jpeg",
+        },
+        {
+            id: 4,
+            name: "about-me.txt",
+            icon: "/images/txt.png",
+            kind: "file",
+            fileType: "txt",
+            position: "top-60 left-5",
+            subtitle: "Meet the Developer Behind the Code",
+            image: "/images/Dharvi.JPG",
+            description: [
+                "Hey! I’m Dharvi 👋, a Full Stack Software Developer currently pursuing my Master’s in Automotive Software Engineering in Germany.",
+                "I build scalable web applications using JavaScript, TypeScript, React, Angular, PHP, and Python — combining clean frontend experiences with solid backend architecture.",
+                "I enjoy designing REST APIs, working with databases like MySQL and MongoDB, and building systems that are structured, maintainable, and actually pleasant to use.",
+                "From simulation-based AI models in autonomous vehicles to real-world full-stack applications, I love turning complex problems into elegant solutions.",
+                "Outside of coding, you’ll probably find me traveling, cooking something experimental, dancing, or reorganizing my workspace like it’s a production deployment 😅"
+            ],
+        },
+    ],
+};
+
+const RESUME_LOCATION = {
+    id: 3,
+    type: "resume",
+    name: "Resume",
+    icon: "/icons/file.svg",
+    kind: "folder",
+    children: [
+        {
+            id: 1,
+            name: "Resume.pdf",
+            icon: "/images/pdf.png",
+            kind: "file",
+            fileType: "pdf",
+            // you can add `href` if you want to open a hosted resume
+            // href: "/your/resume/path.pdf",
+        },
+    ],
+};
+
+const TRASH_LOCATION = {
+    id: 4,
+    type: "trash",
+    name: "Trash",
+    icon: "/icons/trash.svg",
+    kind: "folder",
+    children: [
+        {
+            id: 1,
+            name: "trash1.png",
+            icon: "/images/image.png",
+            kind: "file",
+            fileType: "img",
+            position: "top-10 left-10",
+            imageUrl: "/images/trash-1.png",
+        },
+        {
+            id: 2,
+            name: "trash2.png",
+            icon: "/images/image.png",
+            kind: "file",
+            fileType: "img",
+            position: "top-40 left-80",
+            imageUrl: "/images/trash-2.png",
+        },
+    ],
+};
+
+export const locations = {
+    work: WORK_LOCATION,
+    about: ABOUT_LOCATION,
+    resume: RESUME_LOCATION,
+    trash: TRASH_LOCATION,
+};
+
+const INITIAL_Z_INDEX = 1000;
+
+const WINDOW_CONFIG = {
+    finder: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+    contact: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+    resume: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+    safari: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+    photos: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+    terminal: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+    txtfile: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+    imgfile: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+};
+
+export { INITIAL_Z_INDEX, WINDOW_CONFIG };
