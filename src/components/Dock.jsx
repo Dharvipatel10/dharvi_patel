85e8b965ada6e3c02860ad4f3255614568757571
Dharvipatel10 <dharvipatel1610@gmail.com>
2026-02-20 15:01:26 +0100

    Complete Portfolio features and ready to deploy.

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -2,12 +2,14 @@ import { useRef } from "react";
 import { Tooltip } from "react-tooltip";
 import gsap from "gsap";
 
-import {dockApps} from "#constants/index.js";
+import {dockApps, locations} from "#constants/index.js";
 import {useGSAP} from "@gsap/react";
 import useWindowStore from "#store/window.js";
+import useLocationStore from "#store/location.js";
 
 const Dock = () => {
     const { openWindow, closeWindow, windows } = useWindowStore();
+    const { setActiveLocation } = useLocationStore();
     const dockRef = useRef(null);
 
     useGSAP(() => {
@@ -58,6 +60,12 @@ const Dock = () => {
 
     const toggleApp = (app) => {
         if(!app.canOpen) return;
+
+        if (app.id === "trash") {
+            openWindow("finder");
+            setActiveLocation(locations.trash);
+            return;
+        }
 
         const window = windows[app.id];
 
